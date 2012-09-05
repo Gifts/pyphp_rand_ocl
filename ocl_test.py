f4df601c23bd92c42c7d6268ccf06612392307dc
gifts <gifts.antichat@gmail.com>
2012-09-05 13:43:53 +0400

    Clear code 2

--- ocl_test.py
+++ ocl_test.py
@@ -1,15 +1,14 @@
 #coding: utf8
 
 import pyopencl as cl
-import pyopencl.array as cl_array
 
 import numpy as np
-import numpy.linalg as la
 import time
 
 SIZE = (2**18)
 DIVIDER = 1
 STATE_SIZE = (2**8)
+SIGNIFICANT_LENGTH = 8
 
 
 ## PHP rand constants
@@ -17,7 +16,7 @@ MT_N = 624
 M = 397
 ## END PHP rand constants
 
-MT_state_result = np.zeros((8, SIZE)).astype(np.uint32)
+MT_state_result = np.zeros((SIGNIFICANT_LENGTH, SIZE)).astype(np.uint32)
 
 ctx = cl.create_some_context()
 queue_instruction = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
@@ -33,42 +32,39 @@ prg = cl.Program(ctx, """
     #define STATES_SIZE {1}
     #define M {2}
     #define ALL_SIZE {3}
-    """.format(MT_N, STATE_SIZE, M, SIZE,"#define DIVIDED 1" if DIVIDER > 1 else '') +
+    #define RESULT_LEN {4}
+    """.format(MT_N, STATE_SIZE, M, SIZE, SIGNIFICANT_LENGTH) +
     """
+
     #define hiBit(u)      ((u) & 0x80000000U)  /* mask all but highest   bit of u */
     #define loBit(u)      ((u) & 0x00000001U)  /* mask all but lowest    bit of u */
     #define loBits(u)     ((u) & 0x7FFFFFFFU)  /* mask     the highest   bit of u */
     #define mixBits(u, v) (hiBit(u)|loBits(v)) /* move hi bit of u to hi bit of v */
-
     #define twist(m,u,v)  (m ^ (mixBits(u,v)>>1) ^ ((unsigned int)(-(unsigned int)(loBit(u))) & 0x9908b0dfU))
-    """
 
-    """
 
     __kernel void mt_brute(
         unsigned int seed_start,
-        __global unsigned int *c, //c[MT_N][ALL_SIZE], //
+        __global unsigned int *c,
         __global unsigned int *res
     )
     {
       __private int gid = get_global_id(0);
       __private int lid = get_local_id(0);
 
-      __local unsigned int state[3*STATES_SIZE];
-
 
       /* PHP_MT_VARIABLES*/
+
       __private unsigned int i;
       __private unsigned int s1;
+      __local unsigned int state[3*STATES_SIZE];
+      __private unsigned int s2;
+      __private unsigned int r2;
 
-      //__local unsigned int *next;
       /* END PHP_MT_VARIABLES*/
 
 
       /* PHP_MT_INITIALIZE */
-      //s = state + lid;
-      __private unsigned int s2;
-      __private unsigned int r2;
 
       i = 1;
 
@@ -83,6 +79,7 @@ prg = cl.Program(ctx, """
         r2 = s2;
         c[ALL_SIZE*i + gid] = s2;
       }
+
       /* END PHP_MT_INITIALIZE */
 
 
@@ -97,24 +94,25 @@ prg = cl.Program(ctx, """
         state[2*STATES_SIZE + lid] = c[(i+M) * ALL_SIZE + gid];
         c[ALL_SIZE*i + gid] = twist(state[2*STATES_SIZE + lid], state[lid], state[STATES_SIZE + lid]);
       }
-      r2 = MT_N - M;
+
       for (i = MT_N - M; i < MT_N-1; ++i)
       {
         state[                lid] = state[STATES_SIZE + lid];  // +++ p[0] = c[0][gid]
         state[STATES_SIZE   + lid] = c[(i+1) * ALL_SIZE + gid];
-        state[STATES_SIZE*2 + lid] = c[(i-r2) * ALL_SIZE + gid];
+        state[STATES_SIZE*2 + lid] = c[(i-(MT_N - M)) * ALL_SIZE + gid];
         c[ALL_SIZE*i + gid] = twist(state[2*STATES_SIZE + lid], state[lid], state[STATES_SIZE + lid]);
       }
       state[                lid] = state[STATES_SIZE + lid];  // +++ p[0] = c[0][gid]
       state[STATES_SIZE   + lid] = c[gid];
-      state[STATES_SIZE*2 + lid] = c[(i-r2) * ALL_SIZE + gid];
+      state[STATES_SIZE*2 + lid] = c[(i-(MT_N - M)) * ALL_SIZE + gid];
       c[ALL_SIZE*i + gid] = twist(state[2*STATES_SIZE + lid], state[lid], state[STATES_SIZE + lid]);
 
       /* END PHP_MT_RELOAD */
 
+
       /* PHP_MT_RAND */
 
-      for (i = 0; i < 8; ++i)
+      for (i = 0; i < RESULT_LEN; ++i)
       {
           s1 = c[ALL_SIZE*i + gid];
 
@@ -124,6 +122,7 @@ prg = cl.Program(ctx, """
           s1 ^= (s1 >> 18);
           res[ALL_SIZE*i + gid] = (long)(s1 >> 1);
       }
+
       /* END PHP_MT_RAND */
 
       return;
